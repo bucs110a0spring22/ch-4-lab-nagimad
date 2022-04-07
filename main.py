38a981812b160b98e0789a031fac5953d33b4102
nagima Dubanaeva <98920760+nagimad@users.noreply.github.com>
2022-04-07 17:55:09 +0000

    redid the assignment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import math
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
@@ -7,10 +7,57 @@ import turtle
 # and should review how they work or ask for help
 
 
+def drawSineCurve(my_turtle = None):
+  for x in range(-360,361):
+    y = math.sin(math.radians(x))
+    my_turtle.goto(x,y)
+  my_turtle.up()
 
+def setupWindow(mywindow = None):
+  mywindow.setworldcoordinates(-360, -1, 361, 1)
+  mywindow.bgcolor("pink")
 
+  
+def setupAxis (myturtle = None):
+  myturtle.up()
+  myturtle.goto(-360, 0)
+  myturtle.down()
+  myturtle.goto(361,0)
+  myturtle.up()
+  myturtle.goto(0,-1)
+  myturtle.down()
+  myturtle.goto(0,1)
+  myturtle.up()
+  myturtle.goto(-360,0)
+  myturtle.down()
 
+  
+  
 
+def drawSineCurve(my_turtle = None):
+  my_turtle.goto(-360,0)
+  my_turtle.down()
+  for x in range(-360,361):
+    y = math.sin(math.radians(x))
+    my_turtle.goto(x,y)
+  my_turtle.up()
+  
+  
+def drawCosineCurve(my_turtle = None):
+  my_turtle.goto(-360,0)
+  my_turtle.down()
+  for x in range(-360, 361):
+    y = math.cos(math.radians(x))
+    my_turtle.goto(x,y)
+  my_turtle.up()
+   
+def drawTangentCurve(my_turtle = None):
+  my_turtle.goto(-360,0)
+  my_turtle.down()
+  for x in range(-360, 361):
+    y = math.tan(math.radians(x))
+    my_turtle.goto(x,y)
+  my_turtle.up()
 
 
 ##########  Do Not Alter Any Code Past Here ########
